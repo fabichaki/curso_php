8d4db704d2f885dd7b0a980f2190bfbb0f0f7174
fabichaki <gabriel5558t@gmail.com>
2024-06-18 21:58:32 -0300

    Primeiro commit no github

---
+++ aula1.php
@@ -0,0 +1,24 @@
+<?php
+
+// primeira aula dia 18/06/2024 - turma BG05000
+// conteúdos: "echo", comentários e variáveis.
+// por marcos carraro
+
+
+echo "teste número 1<br>";
+echo "<br>";
+$texto = "pequeno texto que você está lendo sobre o Denis o pimentinha";
+echo $texto;
+echo "<br>";
+$outravariavel = "pequeno grande homem"; // pizza é muito bom
+echo $outravariavel;
+echo "<br>";
+echo "calculos matemáticos ";
+echo "<br>";
+echo 1+1;
+echo "<br>";
+echo 5*9;
+echo "<br>";
+echo 50/5;
+
+?>
