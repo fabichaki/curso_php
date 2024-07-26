2e43258e826960318492dd7a08df53ba6472e4c5
fabichaki <gabriel5558t@gmail.com>
2024-07-26 18:32:36 -0300

    alobeijo

--- z1simulacao.php
+++ z1simulacao.php
@@ -1,18 +1,25 @@
 <?php
 
 echo "teste de texto"; echo "<br>";
-echo date("d/m/Y H:i:s A");
+echo date("d/m/Y H:i:s A"); echo "<br>";
 
-$digito1 = $texto1;
-$digito2 = $texto2;
-$digito3 = $texto3;
-$digito4 = $texto4;
 
 $texto1 = "Olá! Agradecemos por entrar em contato com nosso número! Aqui você irá falar diretamente com um de nossos vendedores!";
+
 $texto2 = "Olá! Agradecemos por entrar em contato com nosso número! Aqui você irá falar diretamente com alguém da nossa equipe de suportes para tirar todas as suas duvidas!";
+
 $texto3 = "Olá! Agradecemos por entrar em contato com nosso número! Aqui você irá falar diretamente com o nosso grupo financeiro, permaneça na linha que logo vamos te atender!";
+
 $texto4 = "Olá! Agradecemos por entrar em contato com nosso número! Aqui você irá falar diretamente com os nossos tecnicos, que irão lhe atender o mais breve o possivel em sua residencia!";
 
-//TELEFONE:
+$a = $texto1;
+$b = $texto2;
+$c = $texto3;
+$d = $texto4;
+
+echo $a; echo"<br>";
+echo $b; echo"<br>";
+echo $c; echo"<br>";
+echo $d; echo"<br>";
 
 ?>
