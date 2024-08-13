efb2dfc46eb1dd13a5675b4ff730f1bcc4889a32
fabichaki <gabriel5558t@gmail.com>
2024-08-13 19:36:21 -0300

    13/08/2024

--- !triangulos.php
+++ !triangulos.php
@@ -1,11 +1,6 @@
 <?php
 
 /*
-- Identificar o tipo de triangulo conforme os daods passados pelo usuario.
-- Sendo tres medidas: $a, $b, $c.
-- Com estas medidas efetuar o calculo e informar ao usuario qual o tipo de 
-triangulo que é formado pelas medidas que ele passou.
-
 - Trianugulos a considerar: equilatero, isoceles ou escaleno.
 
 * equilatero = os três lados possuem o mesmo resultado;
@@ -19,16 +14,16 @@ $lado1 = 0; $lado2 = 0; $lado3 = 0; (triangulo isoceles);
 $lado1 = 0; $lado2 = 0; $lado3 = 0; (triangulo escaleno);
 */
 
-$lado1 = 0;
-$lado2 = 0;
-$lado3 = 0;
+$lado1 = 1;
+$lado2 = 1;  // agora só determinar o número, e sucesso!
+$lado3 = 1;
 
-if($lado1 == $lado2 == $lado3){
+if($lado1 == $lado2 && $lado1 == $lado3 && $lado2 == $lado3){
     echo "Quando os três lados possuem o mesmo tamanho é considerado um triângulo equilátero";
 }
-if($lado1 == $lado2 != $lado3){
+if($lado1 == $lado2 && $lado1 != $lado3 && $lado2 != $lado3){
     echo "Quando dois lados são iguais temos um triângulo isoceles";
 }
-if($lado1 != $lado2 != $lado3){
+if($lado1 != $lado2 && $lado1 != $lado3 && $lado2 != $lado3){
     echo "Tendo três lados, e todos serem indeferente, temos um triângulo escaleno";
 }
