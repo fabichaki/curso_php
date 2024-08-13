ba20863131c8132a2c739c5fba6996c3e5cd5b40
fabichaki <gabriel5558t@gmail.com>
2024-08-13 20:11:48 -0300

    13/08/2024

--- (pasta_para_divisão_de_linha).php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// e aqui se explode o computador
-
-//for($i = 0; $i <= 100; $i--){
-//    echo $i . ", ";
-//}
---
+++ aula8.php
@@ -0,0 +1,35 @@
+<?php 
+
+/*
+function imprimir(){
+    $abc = 10;
+
+    return $abc;
+}
+
+$resultado = imprimir();
+echo $resultado;
+*/
+
+/*
+function imprimir($tabuada){
+    for($t = 0; $t <= 10; $t++){ echo "<br>";
+        for($n = 0; $n <= 10; $n++){
+            echo "$t X $n = ". $t*$n ."<br>";
+        }
+    }
+    
+  
+
+}
+
+imprimir($tabuada);
+*/
+
+function somaNaCalculadora($num1, $num2){
+    $num1 += 1;
+    $num2 += 2;
+
+}
+
+somaNaCalculadora($num1, $num2);
